c0c8addac1d6626a75bdc3c5b46e54f310bba860
elams18 <elamathis18@gmail.com>
2024-05-27 01:32:28 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,14 +12,16 @@ func main() {
 	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	s, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	val := strings.Split(s, "\n")
-	if err == nil {
-		fmt.Printf("%s: command not found\n", val[0])
-	} else {
-		fmt.Println(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		s, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		val := strings.Split(s, "\n")
+		if err == nil {
+			fmt.Printf("%s: command not found\n", val[0])
+		} else {
+			fmt.Println(err)
+		}
 	}
 }
